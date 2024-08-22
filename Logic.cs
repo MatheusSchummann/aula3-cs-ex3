d75e76090a9a473f9850c2ab004101b7b9bfbe3c
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-22 19:24:14 -0300

    Adicionar arquivos de projeto.

---
+++ Logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_3_ex3
+{
+    internal class Logic
+    {
+        public void logistica(int valor) 
+        {
+            int[] notas = { 100, 50, 20, 10, 5, 2, 1 };
+            int valoratual;
+
+            valoratual = valor;
+
+            foreach (int nota in notas)
+            {
+                int aux = 0;
+                while (valoratual >= nota)
+                {
+                    valoratual = valor - nota;
+                    valor = valoratual;
+                    aux++;
+                    if (valoratual == 0)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("{0}-nota(s) de {1}", aux, nota);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_3_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program(); 
+        }
+        public Program() 
+        {
+            Logic logica = new Logic(); 
+
+            Console.Write("Digite o valor: ");
+
+            string svalor = Console.ReadLine();
+            int valor = Convert.ToInt32(svalor);
+
+            logica.logistica(valor);
+
+        }
+    }
+}
